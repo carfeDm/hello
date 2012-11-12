c75420145ed79e02a15913c5aaf97854f7faa94d
carfeDm <juni_02@naver.com>
2012-11-12 21:16:57 +0900

    add 3 more tests

--- src/net/ss/HelloWorld.java
+++ src/net/ss/HelloWorld.java
@@ -3,7 +3,12 @@ package net.ss;
 public class HelloWorld {
 
 	public String getMessage() {
-		return "HelloWorld";
+		return "Hello World";
+	}
+
+	public String getMessage(String name) {
+		if (name==null) return getMessage();
+		return "Hello " + name;
 	}
 
 }
